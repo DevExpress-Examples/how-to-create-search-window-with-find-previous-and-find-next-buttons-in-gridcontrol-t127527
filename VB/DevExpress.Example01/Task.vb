566c2385b07fe08f00d2b2ff0bd2edc365fc7a8e
DevExpressExampleBot <fakeEmail>
2022-12-16 23:13:10 +0400

    Source auto update [skip ci]

--- VB/DevExpress.Example01/Task.vb
+++ VB/DevExpress.Example01/Task.vb
@@ -1,50 +1,13 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-
 Namespace DevExpress.Example01
-	Public Class Task
-		Private privateName As String
-		Public Property Name() As String
-			Get
-				Return privateName
-			End Get
-			Set(ByVal value As String)
-				privateName = value
-			End Set
-		End Property
 
-		Private privateCompany As String
-		Public Property Company() As String
-			Get
-				Return privateCompany
-			End Get
-			Set(ByVal value As String)
-				privateCompany = value
-			End Set
-		End Property
+    Public Class Task
 
-		Private privateSartDate As DateTime
-		Public Property SartDate() As DateTime
-			Get
-				Return privateSartDate
-			End Get
-			Set(ByVal value As DateTime)
-				privateSartDate = value
-			End Set
-		End Property
+        Public Property Name As String
 
-		Private privateIsCompleted As Boolean
-		Public Property IsCompleted() As Boolean
-			Get
-				Return privateIsCompleted
-			End Get
-			Set(ByVal value As Boolean)
-				privateIsCompleted = value
-			End Set
-		End Property
-	End Class
+        Public Property Company As String
+
+        Public Property SartDate As Date
+
+        Public Property IsCompleted As Boolean
+    End Class
 End Namespace
